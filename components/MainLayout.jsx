370f499ded8366c165dcddad949299078bb83a44
Настя <smelw@list.ru>
2022-10-02 22:37:16 +0300

    changed

--- components/MainLayout.jsx
+++ components/MainLayout.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
+/*UI*/
+import UiMainLayout from "./ui/main-layout";
 
 const MainLayout = ({ children }) => {
   return (
-    <>
+    <UiMainLayout>
       <Header />
       {children}
       <Footer />
-    </>
+    </UiMainLayout>
   );
 };
 
---
+++ components/ui/main-layout.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const MainLayout = styled.div`
+  width: 100%;
+  max-width: 1260px;
+  margin: 0 auto;
+`;
+
+const UiMainLayout = Object.assign(MainLayout, {});
+
+export default UiMainLayout;
