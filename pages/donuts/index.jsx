aec4112ef64c629e048df0405c2a4182bceb3949
jmewader <smelw@list.ru>
2024-02-01 22:19:30 +0300

    fix

--- pages/donuts/index.jsx
+++ pages/donuts/index.jsx
@@ -1,76 +1,43 @@
-/*UI*/
 import Image from "next/image";
 import Link from "next/link";
 import styles from "/styles/Donuts.module.css";
 
-// export const getStaticProps = async () => {
-//   const res = await fetch("http://localhost:6000/items");
-//   const data = await res.json();
+export const getStaticProps = async () => {
+  const res = await fetch("http://localhost:5000/items");
+  const data = await res.json();
 
-//   return {
-//     props: { donuts: data },
-//   };
-// };
-
-const Donuts = () => {
-  const donuts = {
-    list: [
-      {
-        name: "Пончик с клубникой",
-        image: "/images/donutstrawberry.jpg",
-        description: "Пончик с натуральной клубничной начинкой и разноцветной посыпкой. Яркий, насыщенный вкус и притягательный аромат этой сладкой выпечки прекрасно дополнит любое чаепитие.",
-        price: "150",
-        id: "1",
-      },
-      {
-        name: "Пончик с шоколадом",
-        image: "/images/donutchoco.jpg",
-        description: "Шоколадный Донат - это классический пончик в шоколадной глазури, который просто тает во рту!",
-        price: "170",
-        id: "2",
-      },
-      {
-        name: "Пончик с ванилью",
-        image: "/images/donutvanilla.jpg",
-        description: "Донат с ванильным кремом – это ароматный пончик с нежной начинкой из ванильного крема, покрытый белой и шоколадной глазурью. ",
-        price: "130",
-        id: "3",
-      },
-    ],
+  return {
+    props: { donuts: data },
   };
+};
 
+const Donuts = ({ donuts }) => {
   return (
     <div>
       <h1>Наши пончики</h1>
 
       <div className={styles.list}>
-        {donuts.list.map((item) => {
-          return <Item key={item.id} {...item} />;
+        {donuts.map((item) => {
+          return (
+            <div key={item.id} className={styles.item}>
+              <div className={styles.info}>
+                <div className={styles.image}>
+                  <Image src={`${item.image}`} alt={`${item.name}`} width="100%" height="100%" layout="responsive" objectFit="cover" />
+                </div>
+
+                <h3 className={styles.title}>{item.name}</h3>
+
+                <p className={styles.text}>{item.description}</p>
+              </div>
+              <div className={styles.wrap}>
+                <div className={styles.price}>{item.price}₽</div>
+                <Link href={`/donuts/${item.id}`}>
+                  <button className={styles.btn}>Подробнее</button>
+                </Link>
+              </div>
+            </div>
+          );
         })}
-      </div>
-    </div>
-  );
-};
-
-const Item = (props) => {
-  const { id, name, description, price, image } = props;
-
-  return (
-    <div className={styles.item}>
-      <div className={styles.info}>
-        <div className={styles.image}>
-          <Image src={`${image}`} alt={`${name}`} width="100%" height="100%" layout="responsive" objectFit="cover" />
-        </div>
-
-        <h3 className={styles.title}>{name}</h3>
-
-        <p className={styles.text}>{description}</p>
-      </div>
-      <div className={styles.wrap}>
-        <div className={styles.price}>{price}₽</div>
-        {/* <Link href={`/donuts/${id}`}>
-          <button className={styles.btn}>Подробнее</button>
-        </Link> */}
       </div>
     </div>
   );
