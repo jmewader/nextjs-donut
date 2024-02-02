bc48c315da6c901709530457f2800efc347908a6
jmewader <smelw@list.ru>
2024-02-02 14:11:44 +0300

    changed data

--- pages/donuts/[id].jsx
+++ pages/donuts/[id].jsx
@@ -2,14 +2,26 @@ import styles from "/styles/Detail.module.css";
 import Image from "next/image";
 
 export const getServerSideProps = async (context) => {
-  const id = context.params.id;
+  try {
+    const id = context.params.id;
+    const res = await fetch(`http://localhost:5000/items/${id}`);
 
-  const res = await fetch(`http://localhost:5000/items/${id}`);
-  const data = await res.json();
+    if (!res.ok) {
+      throw new Error(`Failed to fetch data: ${res.status}`);
+    }
 
-  return {
-    props: { donut: data },
-  };
+    const data = await res.json();
+
+    return {
+      props: { donut: data },
+    };
+  } catch (error) {
+    console.error("Error fetching data:", error.message);
+
+    return {
+      props: { donut: null },
+    };
+  }
 };
 
 const Detail = ({ donut }) => {
@@ -18,14 +30,7 @@ const Detail = ({ donut }) => {
   return (
     <div className={styles.item}>
       <div className={styles.image}>
-        <Image
-          src={`${image}`}
-          alt={`${name}`}
-          width="100%"
-          height="100%"
-          layout="responsive"
-          objectFit="cover"
-        />
+        <Image src={`${image}`} alt={`${name}`} width="100%" height="100%" layout="responsive" objectFit="cover" />
       </div>
       <div className={styles.wrap}>
         <h3 className={styles.title}>{name}</h3>
